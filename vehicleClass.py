2ea6700329492d9b16402210a969493838bf7715
Ankur Kumar Suman <50414074+ankursuman@users.noreply.github.com>
2020-06-19 20:21:14 +0530

    Add files via upload

---
+++ vehicleClass.py
@@ -0,0 +1,7 @@
+class Veichle:
+    def __init__(self,entry_time,exit_time,driver_name,car_plate_number):
+        self.entry_time = entry_time
+        self.exit_time = exit_time
+        self.driver_name = driver_name
+        self.car_plate_number = car_plate_number
+        
