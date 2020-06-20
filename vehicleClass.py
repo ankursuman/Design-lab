b837e211c04885a0c4a298bbf8c00307ab2db2d7
Ankur Kumar Suman <50414074+ankursuman@users.noreply.github.com>
2020-06-20 11:39:37 +0530

    Update vehicleClass.py

--- vehicleClass.py
+++ vehicleClass.py
@@ -5,3 +5,14 @@ class Vehicle:
         self.driver_name = driver_name
         self.car_plate_number = car_plate_number
         
+    def vehicle(self):
+        if(self.entry_time is None):
+            return "entry time cannot be None"
+        elif(self.driver_name is None):
+            return "driver name cannot be None"
+        elif(self.driver_name.isnumeric()):
+            return "driver name cannot be a number"
+        elif(self.car_plate_number is None):
+            return "car plate number cannot be None"
+        else:
+            return "true"
